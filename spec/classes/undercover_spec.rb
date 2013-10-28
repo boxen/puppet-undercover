e3c637fb9a28b4c9b8c00d9ec128e0bc1cb5ea21
Takashi Yoshida <takashi@takashiyoshida.org>
2013-10-28 12:12:02 +0800

    Bump version to 5.5.1

--- spec/classes/undercover_spec.rb
+++ spec/classes/undercover_spec.rb
@@ -4,7 +4,7 @@ describe 'undercover' do
   it do
     should contain_package('Undercover').with({
       :provider => 'pkgdmg',
-      :source   => 'http://assets.undercoverhq.com/client/5.1.2/undercover_mac.pkg'
+      :source   => 'http://assets.undercoverhq.com/client/5.5.1/undercover_mac.pkg'
     })
   end
 end
